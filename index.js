c27494907ed911851895d085d28d59e29b4487a1
mitranikhil44 <mitranikhil93@gmail.com>
2023-12-13 15:11:14 +0530

    Initial commit

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const express = require("express");
 const cheerio = require("cheerio");
 const axios = require("axios");
 const cors = require("cors");
+const cron = require("cron");
 
 // Connet to database
 connectToMongo();
@@ -26,15 +27,15 @@ app.use(express.json());
 app.use(cors());
 
 const handleDataRoute =
-(model, sortParams = { id: -1 }) =>
-async (req, res) => {
-  try {
-    const data = await model.find({}).sort(sortParams);
-    res.json({ success: true, data: data });
-  } catch (err) {
-    res.status(500).json({ success: false, error: err });
-  }
-};
+  (model, sortParams = { id: -1 }) =>
+  async (req, res) => {
+    try {
+      const data = await model.find({}).sort(sortParams);
+      res.json({ success: true, data: data });
+    } catch (err) {
+      res.status(500).json({ success: false, error: err });
+    }
+  };
 
 // Default Route
 app.get("/", (req, res) => {
@@ -58,25 +59,41 @@ app.post("/fin_nifty", fetchUser, handleDataRoute(FinNiftyData));
 app.post("/nifty_option_chain", fetchUser, handleDataRoute(NiftyOptionData));
 
 // Bank Nifty Option Chain Data Route
-app.post("/bank_nifty_option_chain", fetchUser, handleDataRoute(BankNiftyOptionData));
+app.post(
+  "/bank_nifty_option_chain",
+  fetchUser,
+  handleDataRoute(BankNiftyOptionData)
+);
 
 // Fin Nifty Option Chain Data Route
-app.post("/fin_nifty_option_chain", fetchUser, handleDataRoute(FinNiftyOptionData));
+app.post(
+  "/fin_nifty_option_chain",
+  fetchUser,
+  handleDataRoute(FinNiftyOptionData)
+);
 
 // Nifty Market Price Route
 app.post("/nifty_market_price", fetchUser, handleDataRoute(NiftyMarketPrice));
 
 // Bank Nifty Market Price Route
-app.post("/bank_nifty_market_price", fetchUser, handleDataRoute(BankNiftyMarketPrice));
+app.post(
+  "/bank_nifty_market_price",
+  fetchUser,
+  handleDataRoute(BankNiftyMarketPrice)
+);
 
 // Fin Nifty Market Price Route
-app.post("/fin_nifty_market_price", fetchUser, handleDataRoute(FinNiftyMarketPrice));
+app.post(
+  "/fin_nifty_market_price",
+  fetchUser,
+  handleDataRoute(FinNiftyMarketPrice)
+);
 
 const fetchMarketPrice = async (url) => {
   const response = await axios.get(url);
   const $ = cheerio.load(response.data);
   const value = $("bdo").text().trim().substring(0, 9);
-  return value === '-' ? 0 : parseFloat(value.replace(/,/g, ''));
+  return value === "-" ? 0 : parseFloat(value.replace(/,/g, ""));
 };
 
 // Scalped live nifty 50 market price
@@ -86,14 +103,13 @@ const getNiftyPrice = async () => {
 
 // Scalped live bank nifty market price
 const getBankNiftyPrice = async () => {
-  return fetchMarketPrice('https://in.investing.com/indices/bank-nifty');
+  return fetchMarketPrice("https://in.investing.com/indices/bank-nifty");
 };
 
 // Scalped live fin nifty market price
 const getFinNiftyPrice = async () => {
   return fetchMarketPrice(`https://in.investing.com/indices/cnx-finance`);
 };
-
 
 // Function to clear the database
 const clearDatabase = async () => {
@@ -120,20 +136,25 @@ const clearDatabase = async () => {
 };
 
 // Schedule the function to run every day at 12:00am in Indian Standard Time
-const dailyClearDatabaseJob = cron.job('0 0 * * *', async () => {
-    await clearDatabase();
+const dailyClearDatabaseJob = cron.job("0 0 * * *", async () => {
+  await clearDatabase();
 });
-dailyClearDatabaseJob.start();
 
 // Function to set live market price
 const setLiveMarketPrice = async (marketPriceModel, getPriceFn) => {
   try {
     const livePrice = await getPriceFn();
-    const marketData = await marketPriceModel.find().sort({ _id: -1 }).limit(1).exec();
+    const marketData = await marketPriceModel
+      .find()
+      .sort({ _id: -1 })
+      .limit(1)
+      .exec();
 
     if (marketData.length === 0) {
       const newMarketData = {
-        timestamp: new Date().toLocaleTimeString("en-US", { timeZone: "Asia/Kolkata" }),
+        timestamp: new Date().toLocaleTimeString("en-US", {
+          timeZone: "Asia/Kolkata",
+        }),
         price: livePrice,
         volume: 0,
       };
@@ -142,7 +163,9 @@ const setLiveMarketPrice = async (marketPriceModel, getPriceFn) => {
       const lastMarketData = marketData[0];
       const newVolume = parseInt(livePrice) - parseInt(lastMarketData.price);
       const newMarketData = {
-        timestamp: new Date().toLocaleTimeString("en-US", { timeZone: "Asia/Kolkata" }),
+        timestamp: new Date().toLocaleTimeString("en-US", {
+          timeZone: "Asia/Kolkata",
+        }),
         price: livePrice,
         volume: newVolume,
       };
@@ -164,24 +187,35 @@ const setLivePrices = async () => {
   }
 };
 
-
 // Function to fetch nifty and bank nifty data
 const fetchData = async () => {
   const niftyResult = await NiftyOptionData.find({});
   const bankNiftyResult = await BankNiftyOptionData.find({});
   const finNiftyResult = await FinNiftyOptionData.find({});
-  const [niftyPrice, bankNiftyPrice, finNiftyPrice] = await Promise.all([getNiftyPrice(), getBankNiftyPrice(), getFinNiftyPrice()]);
-  const currentDate = new Date().toLocaleTimeString('en-US', {
-    timeZone: 'Asia/Kolkata',
+  const [niftyPrice, bankNiftyPrice, finNiftyPrice] = await Promise.all([
+    getNiftyPrice(),
+    getBankNiftyPrice(),
+    getFinNiftyPrice(),
+  ]);
+  const currentDate = new Date().toLocaleTimeString("en-US", {
+    timeZone: "Asia/Kolkata",
     hour12: true,
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
   });
 
   const niftyData = calculateData(niftyResult, niftyPrice, currentDate);
-  const bankNiftyData = calculateData(bankNiftyResult, bankNiftyPrice, currentDate);
-  const finNiftyData = calculateData(finNiftyResult, finNiftyPrice, currentDate);
+  const bankNiftyData = calculateData(
+    bankNiftyResult,
+    bankNiftyPrice,
+    currentDate
+  );
+  const finNiftyData = calculateData(
+    finNiftyResult,
+    finNiftyPrice,
+    currentDate
+  );
 
   // Insert data into NiftyData and BankNiftyData collections
   await Promise.all([
@@ -205,24 +239,35 @@ const calculateData = (result, price, currentDate) => {
     TotalPutOI: 0,
     TotalPutChgOI: 0,
     liveMarketPrice: price,
-  }; 
+  };
 
   result.forEach((item) => {
-    data.TotalCallLTP += item.CallLTP === '-' ? 0 : parseFloat(item.CallLTP.replace(/,/g, ''));
-    data.TotalCallChgLTP += item.CallChgLTP === '-' ? 0 : parseFloat(item.CallChgLTP.replace(/,/g, ''));
-    data.TotalCallVol += item.CallVol === '-' ? 0 : parseFloat(item.CallVol.replace(/,/g, ''));
-    data.TotalCallOI += item.CallOI === '-' ? 0 : parseFloat(item.CallOI.replace(/,/g, ''));
-    data.TotalCallChgOI += item.CallChgOI === '-' ? 0 : parseFloat(item.CallChgOI.replace(/,/g, ''));
-    data.TotalPutLTP += item.PutLTP === '-' ? 0 : parseFloat(item.PutLTP.replace(/,/g, ''));
-    data.TotalPutChgLTP += item.PutChgLTP === '-' ? 0 : parseFloat(item.PutChgLTP.replace(/,/g, ''));
-    data.TotalPutVol += item.PutVol === '-' ? 0 : parseFloat(item.PutVol.replace(/,/g, ''));
-    data.TotalPutOI += item.PutOI === '-' ? 0 : parseFloat(item.PutOI.replace(/,/g, ''));
-    data.TotalPutChgOI += item.PutChgOI === '-' ? 0 : parseFloat(item.PutChgOI.replace(/,/g, ''));
+    data.TotalCallLTP +=
+      item.CallLTP === "-" ? 0 : parseFloat(item.CallLTP.replace(/,/g, ""));
+    data.TotalCallChgLTP +=
+      item.CallChgLTP === "-"
+        ? 0
+        : parseFloat(item.CallChgLTP.replace(/,/g, ""));
+    data.TotalCallVol +=
+      item.CallVol === "-" ? 0 : parseFloat(item.CallVol.replace(/,/g, ""));
+    data.TotalCallOI +=
+      item.CallOI === "-" ? 0 : parseFloat(item.CallOI.replace(/,/g, ""));
+    data.TotalCallChgOI +=
+      item.CallChgOI === "-" ? 0 : parseFloat(item.CallChgOI.replace(/,/g, ""));
+    data.TotalPutLTP +=
+      item.PutLTP === "-" ? 0 : parseFloat(item.PutLTP.replace(/,/g, ""));
+    data.TotalPutChgLTP +=
+      item.PutChgLTP === "-" ? 0 : parseFloat(item.PutChgLTP.replace(/,/g, ""));
+    data.TotalPutVol +=
+      item.PutVol === "-" ? 0 : parseFloat(item.PutVol.replace(/,/g, ""));
+    data.TotalPutOI +=
+      item.PutOI === "-" ? 0 : parseFloat(item.PutOI.replace(/,/g, ""));
+    data.TotalPutChgOI +=
+      item.PutChgOI === "-" ? 0 : parseFloat(item.PutChgOI.replace(/,/g, ""));
   });
 
   return data;
 };
-
 
 // Function to fetch option chain data from the website
 const fetchOptionChainData = async (url, dataModel) => {
@@ -245,22 +290,24 @@ const fetchOptionChainData = async (url, dataModel) => {
 
   // Extract data from the option chain table and add to array
   const tableRows = $(".table_optionchain table tbody tr");
-  const tableData = tableRows.map((i, element) => {
-    const cells = $(element).find("td");
-    return {
-      CallOI : cells.eq(0).text().trim(),
-      CallChgOI : cells.eq(1).text().trim(),
-      CallVol : cells.eq(2).text().trim(),
-      CallChgLTP : cells.eq(3).text().trim(),
-      CallLTP : cells.eq(4).text().trim(),
-      StrikePrice : cells.eq(5).text().trim(),
-      PutLTP : cells.eq(6).text().trim(),
-      PutChgLTP : cells.eq(7).text().trim(),
-      PutVol : cells.eq(8).text().trim(),
-      PutChgOI : cells.eq(9).text().trim(),
-      PutOI : cells.eq(10).text().trim()
-    };
-  }).get();
+  const tableData = tableRows
+    .map((i, element) => {
+      const cells = $(element).find("td");
+      return {
+        CallOI: cells.eq(0).text().trim(),
+        CallChgOI: cells.eq(1).text().trim(),
+        CallVol: cells.eq(2).text().trim(),
+        CallChgLTP: cells.eq(3).text().trim(),
+        CallLTP: cells.eq(4).text().trim(),
+        StrikePrice: cells.eq(5).text().trim(),
+        PutLTP: cells.eq(6).text().trim(),
+        PutChgLTP: cells.eq(7).text().trim(),
+        PutVol: cells.eq(8).text().trim(),
+        PutChgOI: cells.eq(9).text().trim(),
+        PutOI: cells.eq(10).text().trim(),
+      };
+    })
+    .get();
 
   // Delete all data from collection
   await dataModel.deleteMany({});
@@ -300,7 +347,7 @@ const fetchMarketData = async () => {
     now.day() >= 1 &&
     now.day() <= 5 &&
     now.isBetween(
-      moment.tz("Asia/Kolkata").hour(9).minute(15),
+      moment.tz("Asia/Kolkata").hour(9),
       moment.tz("Asia/Kolkata").hour(15).minute(30),
       "hour",
       "[]"
@@ -317,26 +364,45 @@ const fetchMarketData = async () => {
 };
 
 // Function to fetch live Volume
-const updateLivePrice = async () => { 
+const updateLivePrice = async () => {
   const now = moment().tz("Asia/Kolkata");
   const isMarketOpen =
     now.day() >= 1 &&
     now.day() <= 5 &&
     now.isBetween(
-      moment.tz("Asia/Kolkata").hour(9).minute(15),
+      moment.tz("Asia/Kolkata").hour(9),
       moment.tz("Asia/Kolkata").hour(15).minute(30),
       "hour",
       "[]"
     );
-
   if (isMarketOpen) {
     await setLivePrices();
   }
 };
-  await fetchMarketData();
-  await updateLivePrice();
- 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}}`);
+
+const cronJob = cron.job("*/15 * * * *", async () => {
+  try {
+    await fetchMarketData();
+    await updateLivePrice();
+  } catch (error) {
+    console.error("Error in cron job:", error);
+  }
 });
- 
+
+let currentMin = new Date().getMinutes();
+
+const intervalId = setInterval(() => {
+  const now = new Date();
+  const newMin = now.getMinutes();
+
+  // Check if the minute has changed and is even
+  if (newMin !== currentMin && newMin % 15 === 0) {
+    cronJob.start();
+    dailyClearDatabaseJob.start();
+    clearInterval(intervalId);
+  }
+}, 1000);
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
