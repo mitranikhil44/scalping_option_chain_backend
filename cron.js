a8cc98bdd6980630af59d2d85d397dd603f300ab
mitranikhil44 <mitranikhil93@gmail.com>
2023-12-13 15:22:08 +0530

    Initial commit

--- cron.js
+++
@@ -1,8 +0,0 @@
-export default async function handler(req, res) {
-    // Place your code here to run periodically
-    await fetchMarketData();
-    await updateLivePrice();
-    
-    res.status(200).end('Cron job executed successfully.');
-  }
-  
